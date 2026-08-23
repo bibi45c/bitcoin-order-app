3d9935831f2172de99e5dadfd0a62919f0c5ff35
bibi45c <bibi45gt@gmail.com>
2026-08-23 19:11:05 +0800

    write zap report to workspace

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -5,6 +5,6 @@ docker pull zaproxy/zap-stable
 docker run --rm -v "$(pwd):/zap/wrk/:rw" -t zaproxy/zap-stable \
   zap-baseline.py \
   -t "https://kenken64.github.io/bitcoin-order-app" \
-  -r zap_baseline_report.html
+  -r /zap/wrk/zap_baseline_report.html
 
 echo $? > /dev/null
