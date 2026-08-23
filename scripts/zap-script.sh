9d4d185e8508c03e96415313b013a433742ef299
bibi45c <bibi45gt@gmail.com>
2026-08-23 22:42:17 +0800

    generate zap baseline report

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -2,9 +2,8 @@
 
 docker pull zaproxy/zap-stable
 
-docker run --rm --user root -v "$(pwd):/zap/wrk/:rw" -t zaproxy/zap-stable \
-  zap-baseline.py \
+docker run -i zaproxy/zap-stable zap-baseline.py \
   -t "https://kenken64.github.io/bitcoin-order-app" \
-  -r /zap/wrk/zap_baseline_report.html
+  > zap_baseline_report.html
 
 echo $? > /dev/null
